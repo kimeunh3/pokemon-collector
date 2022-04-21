1927cd695e237dbb88662c5c2766e0a8c9223fec
bgeun <papoke22@gmail.com>
2022-04-21 23:25:57 +0900

    [Feature] 회원가입 UI 추가

--- front/src/components/views/LoginPage/LoginPage.js
+++ front/src/components/views/LoginPage/LoginPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '@mui/material/Container';
 // import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -6,8 +6,10 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 // import Stack from '@mui/material/Stack';
+import RegisterPage from './RegisterPage';
 
 function LoginPage() {
+	const [Login, setLogin] = useState(true);
 	return (
 		<Container
 			component='main'
@@ -20,7 +22,7 @@ function LoginPage() {
 			}}
 		>
 			<Box
-				mt={4}
+				mt={10}
 				sx={{
 					float: 'right',
 					display: 'flex',
@@ -39,47 +41,53 @@ function LoginPage() {
 					<Typography component='h4' variant='h4' mt={4} marginLeft={5} mb={4}>
 						환영합니다!
 					</Typography>
-
-					<Box
-						component='form'
-						sx={{ display: 'flex', flexDirection: 'column' }}
-					>
-						<TextField
-							margin='normal'
-							required
-							id='email'
-							label='이메일'
-							name='email'
-							autoComplete='email'
-							autoFocus
-							sx={{ width: '70%', margin: 'auto', marginBottom: '4%' }}
-						/>
-						<TextField
-							margin='normal'
-							required
-							name='password'
-							label='비밀번호'
-							type='password'
-							id='password'
-							autoComplete='current-password'
-							sx={{ width: '70%', margin: 'auto' }}
-						/>
-						<Button
-							type='submit'
-							variant='contained'
-							color='error'
-							sx={{ width: '40%', margin: 'auto', mt: 3, mb: 2 }}
+					{Login ? (
+						<Box
+							component='form'
+							sx={{ display: 'flex', flexDirection: 'column' }}
 						>
-							로그인
-						</Button>
-						<Button
-							variant='contained'
-							color='success'
-							sx={{ width: '40%', margin: 'auto', mt: 1, mb: 2 }}
-						>
-							회원 가입
-						</Button>
-					</Box>
+							<TextField
+								margin='normal'
+								required
+								id='email'
+								label='이메일'
+								name='email'
+								autoComplete='email'
+								autoFocus
+								sx={{ width: '70%', margin: 'auto', marginBottom: '4%' }}
+							/>
+							<TextField
+								margin='normal'
+								required
+								name='password'
+								label='비밀번호'
+								type='password'
+								id='password'
+								autoComplete='current-password'
+								sx={{ width: '70%', margin: 'auto' }}
+							/>
+							<Button
+								type='submit'
+								variant='contained'
+								color='error'
+								sx={{ width: '40%', margin: 'auto', mt: 3, mb: 2 }}
+							>
+								로그인
+							</Button>
+							<Button
+								variant='contained'
+								color='success'
+								sx={{ width: '40%', margin: 'auto', mt: 1, mb: 2 }}
+								onClick={() => {
+									setLogin(false);
+								}}
+							>
+								회원 가입
+							</Button>
+						</Box>
+					) : (
+						<RegisterPage setLogin={setLogin} />
+					)}
 				</Container>
 			</Box>
 		</Container>
