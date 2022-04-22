df88904ba24982f6e90996dd6d84cfb02334accf
singahyeon12 <singahyeon12@naver.com>
2022-04-22 12:02:33 +0900

    [Feature] NavBar Hamburger Bar 생성

--- front/src/components/views/NavBar/NavBar.js
+++ front/src/components/views/NavBar/NavBar.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 // import { UserStateContext } from '../../Context';
 import {
-  AppBar, Toolbar, IconButton, Button, Grid,
+  Toolbar, IconButton, styled, useTheme, Drawer, Button,
+  List, Divider, ListItem, Grid,
 } from '@material-ui/core';
+import MuiAppBar from '@material-ui/core/AppBar';
+import './NavBar.css';
 
 function NavBar() {
   const navigate = useNavigate();
@@ -28,14 +31,53 @@ function NavBar() {
   //   navigate('/login');
   // };
 
+  const drawerWidth = 240;
+
+  const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+  })(({ theme, open }) => ({
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+      width: '100%',
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginRight: 0,
+    }),
+  }));
+
+  const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-start',
+  }));
+
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div>
-      <AppBar position="fixed" style={{ backgroundColor: '#D72A1F', boxShadow: '0 30px black' }}>
+      <AppBar position="fixed" open={open} style={{ backgroundColor: '#D72A1F', boxShadow: '0 30px black' }}>
         <Toolbar>
           <IconButton edge="start" color="inherit" aria-label="menu" onClick={() => navigate('/login')} style={{ marginRight: '40%' }}>
             <img alt="" src={require('./logo.png')} width="200px" height="70px" />
           </IconButton>
-          <Grid container>
+          <Grid container id="menu">
             <Grid item xs={2}>
               <Button color="inherit" onClick={() => navigate('/login')} style={{ fontSize: '18px' }}>포켓몬빵</Button>
             </Grid>
@@ -55,8 +97,68 @@ function NavBar() {
               <Button color="primary" onClick={() => navigate('/login')} style={{ fontSize: '18px' }}>로그아웃</Button>
             </Grid>
           </Grid>
+          <IconButton
+            id="Hamburger"
+            color="info"
+            aria-label="open drawer"
+            edge="end"
+            onClick={handleDrawerOpen}
+            sx={{ ...(open && { display: 'none' }) }}
+            style={{
+              marginRight: '5%', marginLeft: 'auto',
+            }}
+          >
+            <span className="material-symbols-outlined">
+              menu
+            </span>
+          </IconButton>
         </Toolbar>
       </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+          },
+        }}
+        variant="persistent"
+        anchor="right"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <span className="material-symbols-outlined">menu</span> : <span className="material-symbols-outlined">close</span>}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          <ListItem>
+            <span className="material-symbols-outlined">arrow_right</span>
+            <Button color="inherit" onClick={() => navigate('/login')} style={{ fontSize: '18px' }}>포켓몬빵</Button>
+          </ListItem>
+          <ListItem>
+            <span className="material-symbols-outlined">arrow_right</span>
+            <Button color="inherit" onClick={() => navigate('/login')} style={{ fontSize: '18px' }}>퀴즈</Button>
+          </ListItem>
+          <ListItem>
+            <span className="material-symbols-outlined">arrow_right</span>
+            <Button color="inherit" onClick={() => navigate('/login')} style={{ fontSize: '18px' }}>도감</Button>
+          </ListItem>
+          <ListItem>
+            <span className="material-symbols-outlined">arrow_right</span>
+            <Button color="inherit" onClick={() => navigate('/login')} style={{ fontSize: '18px' }}>통계</Button>
+          </ListItem>
+          <ListItem>
+            <span className="material-symbols-outlined">arrow_right</span>
+            <Button color="inherit" onClick={() => navigate('/login')} style={{ fontSize: '18px' }}>마이페이지</Button>
+          </ListItem>
+          <ListItem>
+            <span className="material-symbols-outlined">arrow_right</span>
+            <Button color="primary" onClick={() => navigate('/login')} style={{ fontSize: '18px' }}>로그아웃</Button>
+          </ListItem>
+        </List>
+      </Drawer>
       <div style={{
         position: 'fixed', top: '54px', width: '100%', zIndex: '9999',
       }}
