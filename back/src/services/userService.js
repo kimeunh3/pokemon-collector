af6c2b7f98b054e6933cc1e063f91e5a4dbd57f7
[4기-2반] 장소영 <soy5360@naver.com>
2022-04-22 21:29:00 +0900

    [Feature] UserAPI 추가

---
+++ back/src/services/userService.js
@@ -0,0 +1,80 @@
+import is from "@sindresorhus/is";
+import { Router } from "express";
+import { login_required } from "../middlewares/login_required";
+import { userAuthService } from "../services/userService";
+import { UserModel } from '../schemas/user';
+
+const userAuthRouter = Router();
+
+userAuthRouter.post("/user/register", async function (req, res, next) {
+  try {
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "headers의 Content-Type을 application/json으로 설정해주세요"
+      );
+    }
+
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const newUser = await userAuthService.addUser({
+      name,
+      email,
+      password,
+    });
+
+    if (newUser.errorMessage) {
+      throw new Error(newUser.errorMessage);
+    }
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+userAuthRouter.post("/user/login", async function (req, res, next) {
+  try {
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+
+    const user = await userAuthService.getUser({ email, password });
+
+    if (user.errorMessage) {
+      throw new Error(user.errorMessage);
+    }
+
+    res.status(200).send(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+userAuthRouter.get(
+  "/user/current",
+  login_required,
+  async function (req, res, next) {
+    try {
+
+      const user_id = req.currentUserId;
+      const currentUserInfo = await userAuthService.getUserInfo({
+        user_id,
+      });
+
+      if (currentUserInfo.errorMessage) {
+        throw new Error(currentUserInfo.errorMessage);
+      }
+
+      res.status(200).send(currentUserInfo);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+
+export { userAuthRouter };
