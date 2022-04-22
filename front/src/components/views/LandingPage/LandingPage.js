c8ddf45951a6b7a550761b4e88249f3194b71cb9
singahyeon12 <singahyeon12@naver.com>
2022-04-22 18:07:20 +0900

    [Feature] LandingPage(홈) 햄버거바 추가

--- front/src/components/views/LandingPage/LandingPage.js
+++ front/src/components/views/LandingPage/LandingPage.js
@@ -1,19 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // import { UserStateContext } from '../../Context';
 import {
-  Toolbar, AppBar, Button, Grid,
+  Toolbar, IconButton, styled, useTheme, Drawer, Button, Grid,
+  List, Divider, ListItem,
 } from '@material-ui/core';
+import MuiAppBar from '@material-ui/core/AppBar';
 import './LandingPage.css';
 
 function LandingPage() {
   const navigate = useNavigate();
+
+  const drawerWidth = 240;
+
+  const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+  })(({ theme, open }) => ({
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+      width: '100%',
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginRight: 0,
+    }),
+  }));
+
+  const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-start',
+  }));
+
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div>
       <AppBar
         id="Header"
         position="static"
+        open={open}
         style={{
           backgroundColor: '#D72A1F', boxShadow: '0 60px black, inset 0px 3px 4px 30px rgba(0, 0, 0, 0.05)', height: '450px', justifyContent: 'center', alignItems: 'center',
         }}
@@ -39,6 +81,21 @@ function LandingPage() {
               <Button color="primary" onClick={() => navigate('/login')} style={{ fontSize: '30px' }}>로그아웃</Button>
             </Grid>
           </Grid>
+          <IconButton
+            id="Hamburger"
+            color="info"
+            aria-label="open drawer"
+            edge="end"
+            onClick={handleDrawerOpen}
+            sx={{ ...(open && { display: 'none' }) }}
+            style={{
+              marginRight: '5%', marginLeft: 'auto',
+            }}
+          >
+            <span className="material-symbols-outlined">
+              menu
+            </span>
+          </IconButton>
         </Toolbar>
         <img alt="" src={require('../images/logo.png')} width="600px" height="210px" />
       </AppBar>
@@ -50,6 +107,51 @@ function LandingPage() {
           <img alt="" src={require('../images/nav-icon.png')} width="200px" height="200px" />
         </div>
       </div>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+          },
+        }}
+        variant="persistent"
+        anchor="right"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <span className="material-symbols-outlined">menu</span> : <span className="material-symbols-outlined">close</span>}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          <ListItem>
+            <span className="material-symbols-outlined">arrow_right</span>
+            <Button color="inherit" onClick={() => navigate('/login')} style={{ fontSize: '18px' }}>포켓몬빵</Button>
+          </ListItem>
+          <ListItem>
+            <span className="material-symbols-outlined">arrow_right</span>
+            <Button color="inherit" onClick={() => navigate('/login')} style={{ fontSize: '18px' }}>퀴즈</Button>
+          </ListItem>
+          <ListItem>
+            <span className="material-symbols-outlined">arrow_right</span>
+            <Button color="inherit" onClick={() => navigate('/login')} style={{ fontSize: '18px' }}>도감</Button>
+          </ListItem>
+          <ListItem>
+            <span className="material-symbols-outlined">arrow_right</span>
+            <Button color="inherit" onClick={() => navigate('/login')} style={{ fontSize: '18px' }}>통계</Button>
+          </ListItem>
+          <ListItem>
+            <span className="material-symbols-outlined">arrow_right</span>
+            <Button color="inherit" onClick={() => navigate('/login')} style={{ fontSize: '18px' }}>마이페이지</Button>
+          </ListItem>
+          <ListItem>
+            <span className="material-symbols-outlined">arrow_right</span>
+            <Button color="primary" onClick={() => navigate('/login')} style={{ fontSize: '18px' }}>로그아웃</Button>
+          </ListItem>
+        </List>
+      </Drawer>
       <button type="button" id="go-top" onClick={() => window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })}><span className="material-symbols-outlined">arrow_upward</span></button>
       <div style={{
         position: 'relative', top: '200px', left: '10%',
