b3ca79638c745a5e8fba7fa8b16014b53c7adcde
[4기-2반] 장소영 <soy5360@naver.com>
2022-04-23 11:34:27 +0900

    [Modify] index User 모델 추가

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-
+import { User } from './models/User';
 const DB_URL = process.env.MONGODB_URL
   || 'MongoDB 서버 주소가 설정되지 않았습니다.\n./db/index.ts 파일을 확인해 주세요.';
 
@@ -8,3 +8,5 @@ const db = mongoose.connection;
 
 db.on('connected', () => console.log(`정상적으로 MongoDB 서버에 연결되었습니다.  ${DB_URL}`));
 db.on('error', (error) => console.error(`MongoDB 연결에 실패하였습니다...\n${DB_URL}\n${error}`));
+
+export {User};
