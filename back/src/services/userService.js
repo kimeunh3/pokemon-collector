cfd064097f78fd195a299e9a61104027ed98931a
[4기-2반] 장소영 <soy5360@naver.com>
2022-04-23 10:47:43 +0900

    [Modify] userService 수정

--- back/src/services/userService.js
+++ back/src/services/userService.js
@@ -1,85 +1,120 @@
-import is from "@sindresorhus/is";
-import { Router } from "express";
-import { login_required } from "../middlewares/login_required";
-import { userAuthService } from "../services/userService";
+import { User } from "../db"; // from을 폴더(db) 로 설정 시, 디폴트로 index.js 로부터 import함.
+import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
+import jwt from "jsonwebtoken";
 
+class userAuthService {
+  static async addUser({ nickname, email, password, sex, age, interest, likeType, point, profileImg, stickers }) {
+    const user = await User.findByEmail({ email });
+    if (user) {
+      const errorMessage =
+        "이 이메일은 현재 사용중입니다. 다른 이메일을 입력해 주세요.";
+      return { errorMessage };
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-const userAuthRouter = Router();
+    const id = uuidv4();
+    const newUser = { id, nickname, email, password: hashedPassword };
 
-userAuthRouter.post("/user/register", async function (req, res, next) {
-  try {
-    if (is.emptyObject(req.body)) {
-      throw new Error(
-        "headers의 Content-Type을 application/json으로 설정해주세요"
-      );
+    const createdNewUser = await User.create({ newUser });
+    createdNewUser.errorMessage = null;
+    return createdNewUser;
+  }
+
+  static async getUser({ email, password }) {
+    // 이메일 db에 존재 여부 확인
+    const user = await User.findByEmail({ email });
+    if (!user) {
+      const errorMessage =
+        "해당 이메일은 가입 내역이 없습니다. 다시 한 번 확인해 주세요.";
+      return { errorMessage };
     }
 
-    const {nickname, email, password, sex, age, interest, likeType, point, profileImg, stickers} = req.body
-
-    const newUser = await userAuthService.addUser({
-      nickname,
-      email,
+    // 비밀번호 일치 여부 확인
+    const correctPasswordHash = user.password;
+    const isPasswordCorrect = await bcrypt.compare(
       password,
-      sex,
-      age,
-      interest,
-      likeType,
-      point,
-      profileImg,
-      stickers,
-    });
-
-    if (newUser.errorMessage) {
-      throw new Error(newUser.errorMessage);
+      correctPasswordHash
+    );
+    if (!isPasswordCorrect) {
+      const errorMessage =
+        "비밀번호가 일치하지 않습니다. 다시 한 번 확인해 주세요.";
+      return { errorMessage };
     }
 
-    res.status(201).json(newUser);
-  } catch (error) {
-    next(error);
+    const secretKey = process.env.JWT_SECRET_KEY || "jwt-secret-key";
+    const token = jwt.sign({ user_id: user.id }, secretKey);
+
+    const id = user.id;
+    const name = user.name;
+    const description = user.description;
+
+    const loginUser = {
+      token,
+      id,
+      email,
+      name,
+      description,
+      errorMessage: null,
+    };
+
+    return loginUser;
   }
-});
 
-userAuthRouter.post("/user/login", async function (req, res, next) {
-  try {
+  static async getUsers() {
+    const users = await User.findAll();
+    return users;
+  }
 
-    const email = req.body.email;
-    const password = req.body.password;
+  static async setUser({ user_id, toUpdate }) {
+
+    let user = await User.findById({ user_id });
 
 
-    const user = await userAuthService.getUser({ email, password });
-
-    if (user.errorMessage) {
-      throw new Error(user.errorMessage);
+    if (!user) {
+      const errorMessage =
+        "가입 내역이 없습니다. 다시 한 번 확인해 주세요.";
+      return { errorMessage };
     }
 
-    res.status(200).send(user);
-  } catch (error) {
-    next(error);
-  }
-});
-
-
-userAuthRouter.get(
-  "/user/current",
-  login_required,
-  async function (req, res, next) {
-    try {
-
-      const user_id = req.currentUserId;
-      const currentUserInfo = await userAuthService.getUserInfo({
-        user_id,
-      });
-
-      if (currentUserInfo.errorMessage) {
-        throw new Error(currentUserInfo.errorMessage);
-      }
-
-      res.status(200).send(currentUserInfo);
-    } catch (error) {
-      next(error);
+    if (toUpdate.name) {
+      const fieldToUpdate = "name";
+      const newValue = toUpdate.name;
+      user = await User.update({ user_id, fieldToUpdate, newValue });
     }
+
+    if (toUpdate.email) {
+      const fieldToUpdate = "email";
+      const newValue = toUpdate.email;
+      user = await User.update({ user_id, fieldToUpdate, newValue });
+    }
+
+    if (toUpdate.password) {
+      const fieldToUpdate = "password";
+      const newValue = toUpdate.password;
+      user = await User.update({ user_id, fieldToUpdate, newValue });
+    }
+
+    if (toUpdate.description) {
+      const fieldToUpdate = "description";
+      const newValue = toUpdate.description;
+      user = await User.update({ user_id, fieldToUpdate, newValue });
+    }
+
+    return user;
   }
-);
 
+  static async getUserInfo({ user_id }) {
+    const user = await User.findById({ user_id });
 
-export { userAuthRouter };
+    if (!user) {
+      const errorMessage =
+        "해당 이메일은 가입 내역이 없습니다. 다시 한 번 확인해 주세요.";
+      return { errorMessage };
+    }
+
+    return user;
+  }
+}
+
+export { userAuthService };
