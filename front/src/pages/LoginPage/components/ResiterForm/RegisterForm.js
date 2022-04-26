3418cd51a518de3f2860df76848c06b3e574e688
bgeun <papoke22@gmail.com>
2022-04-26 17:07:16 +0900

    [Feature] 회원가입 기능 완성

--- front/src/pages/LoginPage/components/ResiterForm/RegisterForm.js
+++ front/src/pages/LoginPage/components/ResiterForm/RegisterForm.js
@@ -1,57 +1,80 @@
 import React, { useState } from 'react';
-import Box from '@mui/material/Box';
-// import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import Rating from '@mui/material/Rating';
-import Typography from '@mui/material/Typography';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import InputLabel from '@mui/material/InputLabel';
+import {
+	Box,
+	Button,
+	TextField,
+	Grid,
+	Radio,
+	RadioGroup,
+	FormControlLabel,
+	FormControl,
+	InputLabel,
+	Select,
+	MenuItem,
+	Typography,
+	Rating,
+	FormLabel,
+} from '@mui/material';
+import {
+	Adjust,
+	LocalFireDepartment,
+	Opacity,
+	Grass,
+	Bolt,
+	BugReport,
+	CatchingPokemon,
+	AcUnit,
+	SportsMma,
+	Coronavirus,
+	Landscape,
+	Air,
+	Storm,
+	Castle,
+	DarkMode,
+	Adb,
+	Hardware,
+	Reddit,
+} from '@mui/icons-material';
 import * as Api from '../../../../api';
 
 function RegisterPage({ setLogin }) {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const [nickname, setNickname] = useState('');
-	const [interest, setInterest] = React.useState(3);
-	const [favAtt, setFavAtt] = React.useState('');
+	const [inputs, setInputs] = useState({
+		email: '',
+		password: '',
+		nickname: '',
+		sex: 'male',
+		age: '',
+		interest: 3,
+		likeType: '',
+	});
 
+	const onChange = (e) => {
+		const { name, value } = e.target;
+		setInputs({
+			...inputs,
+			[name]: value,
+		});
+	};
+
+	console.log(inputs);
 	const validateEmail = (email) =>
 		email
 			.toLowerCase()
 			.match(
 				/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 			);
-	const isEmailValid = validateEmail(email);
+	const isEmailValid = validateEmail(inputs.email);
 	// 비밀번호가 4글자 이상인지 여부를 확인함.
-	const isPasswordValid = password.length >= 4;
+	const isPasswordValid = inputs.password.length >= 4;
 	// 이메일과 비밀번호 조건이 동시에 만족되는지 확인함.
 	const isFormValid = isEmailValid && isPasswordValid;
 
-	const handleChange = (event) => {
-		setFavAtt(event.target.value);
-	};
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 
 		try {
 			// "user/register" 엔드포인트로 post요청함.
-			await Api.post('user/register', {
-				email,
-				password,
-				nickname,
-				sex: 'Female',
-				age: 25,
-				interest: 5,
-				likeType: '물',
-			});
+			await Api.post('user/register', inputs);
 
 			// 로그인 페이지로 이동함.
 			setLogin(true);
@@ -74,7 +97,7 @@ function RegisterPage({ setLogin }) {
 				label='이메일'
 				name='email'
 				autoComplete='email'
-				onChange={(e) => setEmail(e.target.value)}
+				onChange={onChange}
 				sx={{
 					width: '70%',
 					margin: 'auto',
@@ -98,7 +121,7 @@ function RegisterPage({ setLogin }) {
 				type='password'
 				id='password'
 				autoComplete='current-password'
-				onChange={(e) => setPassword(e.target.value)}
+				onChange={onChange}
 				sx={{ width: '70%', margin: 'auto', marginBottom: '2%' }}
 			/>
 			{!isPasswordValid && (
@@ -121,16 +144,20 @@ function RegisterPage({ setLogin }) {
 				label='닉네임'
 				name='nickname'
 				autoComplete='nickname'
-				onChange={(e) => setNickname(e.target.value)}
+				onChange={onChange}
 				sx={{ width: '70%', margin: 'auto', marginBottom: '1%' }}
 			/>
 			<Grid container spacing={2} sx={{ width: '90%', margin: 'auto' }}>
 				<Grid item xs={6} sm={6}>
 					<FormControl sx={{ margin: 'auto' }} required>
 						<FormLabel>성별</FormLabel>
-						<RadioGroup row defaultValue='남성'>
-							<FormControlLabel value='남성' control={<Radio />} label='남성' />
-							<FormControlLabel value='여성' control={<Radio />} label='여성' />
+						<RadioGroup row defaultValue='male' name='sex' onChange={onChange}>
+							<FormControlLabel value='male' control={<Radio />} label='남성' />
+							<FormControlLabel
+								value='female'
+								control={<Radio />}
+								label='여성'
+							/>
 						</RadioGroup>
 					</FormControl>
 				</Grid>
@@ -139,38 +166,94 @@ function RegisterPage({ setLogin }) {
 						required
 						id='filled-number'
 						label='나이'
-						type='number'
+						name='age'
 						InputLabelProps={{
 							shrink: true,
 						}}
 						variant='standard'
 						size='small'
+						onChange={onChange}
 					/>
 				</Grid>
 
 				<Grid item xs={6} sm={6}>
-					<Typography component='legend'>포켓몬 관심도</Typography>
+					<Typography component='legend'>
+						<CatchingPokemon /> <span> 포켓몬 관심도</span>
+					</Typography>
 					<Rating
-						name='simple-controlled'
-						value={interest}
-						onChange={(event, newValue) => {
-							setInterest(newValue);
-						}}
+						type='number'
+						name='interest'
+						value={Number(inputs.interest)}
+						onChange={onChange}
 					/>
 				</Grid>
 				<Grid item xs={6} sm={5}>
 					<FormControl required fullWidth variant='standard' size='small'>
-						<InputLabel id='demo-simple-select-label'>선호 속성</InputLabel>
+						<InputLabel id='demo-simple-select-label'>선호하는 속성</InputLabel>
 						<Select
 							labelId='demo-simple-select-label'
 							id='demo-simple-select'
-							value={favAtt}
+							name='likeType'
+							value={inputs.likeType}
 							label='favAtt'
-							onChange={handleChange}
+							onChange={onChange}
 						>
-							<MenuItem value={10}>Ten</MenuItem>
-							<MenuItem value={20}>Twenty</MenuItem>
-							<MenuItem value={30}>Thirty</MenuItem>
+							<MenuItem value='노말'>
+								<Adjust /> <span> &nbsp; 노말</span>
+							</MenuItem>
+							<MenuItem value='불꽃'>
+								<LocalFireDepartment /> <span> &nbsp; 불꽃</span>
+							</MenuItem>
+							<MenuItem value='물'>
+								<Opacity />
+								<span> &nbsp; 물</span>
+							</MenuItem>
+							<MenuItem value='풀'>
+								<Grass />
+								<span> &nbsp; 풀</span>
+							</MenuItem>
+							<MenuItem value='전기'>
+								<Bolt />
+								<span> &nbsp; 전기</span>
+							</MenuItem>
+							<MenuItem value='얼음'>
+								<AcUnit />
+								<span> &nbsp; 얼음</span>
+							</MenuItem>
+							<MenuItem value='격투'>
+								<SportsMma /> <span> &nbsp; 격투</span>
+							</MenuItem>
+							<MenuItem value='독'>
+								<Coronavirus /> <span> &nbsp; 독</span>
+							</MenuItem>
+							<MenuItem value='땅'>
+								<Landscape /> <span> &nbsp; 땅</span>
+							</MenuItem>
+							<MenuItem value='비행'>
+								<Air /> <span> &nbsp; 비행</span>
+							</MenuItem>
+							<MenuItem value='에스파'>
+								<Storm /> <span> &nbsp; 에스파</span>
+							</MenuItem>
+							<MenuItem value='벌레'>
+								<BugReport />
+								<span> &nbsp; 벌레</span>
+							</MenuItem>
+							<MenuItem value='바위'>
+								<Castle /> <span> &nbsp; 바위</span>
+							</MenuItem>
+							<MenuItem value='고스트'>
+								<DarkMode /> <span> &nbsp; 고스트</span>
+							</MenuItem>
+							<MenuItem value='드래곤'>
+								<Adb /> <span> &nbsp; 드래곤</span>
+							</MenuItem>
+							<MenuItem value='강철'>
+								<Hardware /> <span> &nbsp; 강철</span>
+							</MenuItem>
+							<MenuItem value='페어리'>
+								<Reddit /> <span> &nbsp; 페어리</span>
+							</MenuItem>
 						</Select>
 					</FormControl>
 				</Grid>
