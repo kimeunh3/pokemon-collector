761cde214468c2ddfd1d57bb541978369d1e1f30
[4기-2반] 장소영 <soy5360@naver.com>
2022-04-27 17:00:09 +0900

    [Modify] 포트번호 수정

--- back/index.js
+++ back/index.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 // import { DefaultDeserializer } from 'v8';
 import { app } from './src/app';
 
-const PORT = process.env.SERVER_PORT || 5001;
+const PORT = process.env.SERVER_PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
