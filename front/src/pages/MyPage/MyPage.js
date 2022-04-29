5b4cf2ba86ce3ba9c3e7ce7a50e4c8aa3d6573aa
bgeun <papoke22@gmail.com>
2022-04-29 16:01:23 +0900

    [Feature] 마이페이지  유저카드에 선호속성, 포인트 추가 업적 칸 추가

--- front/src/pages/MyPage/MyPage.js
+++ front/src/pages/MyPage/MyPage.js
@@ -1,54 +1,31 @@
-import React from 'react';
-import {
-	Container,
-	Card,
-	Avatar,
-	IconButton,
-	CardActions,
-	CardHeader,
-	CardMedia,
-	CardContent,
-	Typography,
-} from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Container, Grid, Box } from '@mui/material';
 
-import { Favorite, Share } from '@mui/icons-material';
+import UserCard from './components/UserCard/UserCard';
 
-function MyPage({ userState }) {
-	const { email } = userState.user;
-	console.log(userState);
-	const profileImg = `https://d31z0g5vo6ghmg.cloudfront.net/pokemons/pokeball.png`;
+import * as Api from '../../api';
+
+function MyPage() {
+	const [userState, setUserState] = useState([]);
+
+	useEffect(() => {
+		const fetchUserInfo = async () => {
+			const res = await Api.get('user/current');
+			await setUserState(res.data);
+		};
+		fetchUserInfo();
+	}, []);
 
 	return (
-		<Container sx={{ marginTop: '165px' }}>
-			<Card sx={{ maxWidth: 345 }}>
-				<CardHeader
-					avatar={<Avatar aria-label='recipe'>R</Avatar>}
-					action={<IconButton aria-label='settings' />}
-					title={email}
-					subheader={email}
-				/>
-				<CardMedia
-					component='img'
-					image={profileImg}
-					alt='profileImg'
-					sx={{ width: '65%', height: '65%', margin: 'auto' }}
-				/>
-				<CardContent>
-					<Typography variant='body2' color='text.secondary'>
-						This impressive paella is a perfect party dish and a fun meal to
-						cook together with your guests. Add 1 cup of frozen peas along with
-						the mussels, if you like.
-					</Typography>
-				</CardContent>
-				<CardActions disableSpacing>
-					<IconButton aria-label='add to favorites'>
-						<Favorite />
-					</IconButton>
-					<IconButton aria-label='share'>
-						<Share />
-					</IconButton>
-				</CardActions>
-			</Card>
+		<Container sx={{ marginTop: '165px', width: '100%' }}>
+			<Grid container>
+				<Grid item xs={6} md={6}>
+					<UserCard userState={userState} />
+				</Grid>
+				<Grid item xs={6} md={6} sx={{ backgroundColor: 'white' }}>
+					<Box>업적</Box>
+				</Grid>
+			</Grid>
 		</Container>
 	);
 }
---
+++ front/src/pages/MyPage/components/UserCard/UserCard.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+	Card,
+	Avatar,
+	IconButton,
+	CardActions,
+	CardHeader,
+	CardMedia,
+	CardContent,
+	Typography,
+} from '@mui/material';
+import { Favorite, Share } from '@mui/icons-material';
+import IconObj from '../../../../core/Icon';
+
+function UserCard({ userState }) {
+	const { email, nickname, likeType, point } = userState;
+
+	const profileImg = `https://d31z0g5vo6ghmg.cloudfront.net/pokemons/pokeball.png`;
+
+	return (
+		<Card sx={{ maxWidth: 345 }}>
+			<CardHeader
+				avatar={
+					<Avatar
+						sx={{
+							bgcolor: 'transparent',
+							border: `3px solid ${IconObj[likeType].Color.color}`,
+						}}
+					>
+						{IconObj[likeType].Icon}
+					</Avatar>
+				}
+				action={<IconButton aria-label='settings' />}
+				title={email}
+				subheader={nickname}
+			/>
+			<CardMedia
+				component='img'
+				image={profileImg}
+				alt='profileImg'
+				sx={{ width: '65%', height: '65%', margin: 'auto' }}
+			/>
+			<CardContent>
+				<Typography variant='body2' color='text.secondary'>
+					{likeType} {point}
+				</Typography>
+			</CardContent>
+			<CardActions disableSpacing>
+				<IconButton aria-label='add to favorites'>
+					<Favorite />
+				</IconButton>
+				<IconButton aria-label='share'>
+					<Share />
+				</IconButton>
+			</CardActions>
+		</Card>
+	);
+}
+
+export default UserCard;
