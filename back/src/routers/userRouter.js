55f2236d176734a84b3790583738c1bfd6466fe8
[4기-2반] 장소영 <soy5360@naver.com>
2022-05-03 21:09:12 +0900

    [ETC] swagger user/register 명세서 작성

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -1,3 +1,10 @@
+import is from '@sindresorhus/is';
+import { Router } from 'express';
+import { loginRequired } from '../middlewares/loginRequired';
+import { userAuthService } from '../services/userService';
+
+const userAuthRouter = Router();
+
 /**
  * @swagger
  * /user/register:
@@ -5,6 +12,46 @@
  *     summary: 회원가입
  *     description: User의 회원가입 API
  *     tags: [register]
+ *     parameters:
+ *       - name: nickname
+ *         in: body
+ *         description: User의 닉네임
+ *         example: 포켓몬 트레이너
+ *       - name: email
+ *         in: body
+ *         description: User의 이메일
+ *         example: a@a.com
+ *       - name: password
+ *         in: body
+ *         description: User의 비밀번호(4글자 이상)
+ *         example: 1234
+ *       - name: sex
+ *         in: body
+ *         description: User의 성별
+ *         example: Male
+ *       - name: birth
+ *         in: body
+ *         description: User의 생년월일
+ *         example: 1988-02-12
+ *       - name: interest
+ *         in: body
+ *         description: User의 포켓몬에 대한 관심도
+ *         example: 3
+ *       - name: likeType
+ *         in: body
+ *         description: User의 좋아하는 포켓몬 속성
+ *         example: 불
+ *       - name: point
+ *         in: body
+ *         description: User의 포인트
+ *         example: 1000
+ *       - name: profileImg
+ *         in: body
+ *         description: User의 프로필사진
+ *         example: pokeball.png
+ *       - name: stickers
+ *         in: body
+ *         description: User의 스티커 목록
  *     responses:
  *       '200':
  *        description: User의 회원가입
@@ -13,14 +60,6 @@
  *           schema:
  *             type: Object
  */
-
-import is from '@sindresorhus/is';
-import { Router } from 'express';
-import { loginRequired } from '../middlewares/loginRequired';
-import { userAuthService } from '../services/userService';
-import { User } from '../db';
-const userAuthRouter = Router();
-
 userAuthRouter.post('/user/register', async function (req, res, next) {
   try {
     if (is.emptyObject(req.body)) {
