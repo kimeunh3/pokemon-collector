b2ed7a1b6efe0d9163a9e006fa0afe6151f040df
singahyeon12 <singahyeon12@naver.com>
2022-05-05 14:19:45 +0900

    [Modify] 능력치차트 코드정리

--- front/src/pages/StatisticsPage/components/StatisticsCharts/StatsStatisticsCharts.js
+++ front/src/pages/StatisticsPage/components/StatisticsCharts/StatsStatisticsCharts.js
@@ -19,7 +19,7 @@ ChartJS.register(
   Legend
 );
 
-export const optionBarTypeStats = {
+const OPTION_BAR_TYPE_STATS = {
   responsive: true,
   plugins: {
     legend: {
@@ -33,7 +33,7 @@ export const optionBarTypeStats = {
   },
 };
 
-export const optionBarTypeTop5 = {
+const OPTION_BAR_TYPE_TOP5 = {
   indexAxis: 'y',
   elements: {
     bar: {
@@ -53,7 +53,7 @@ export const optionBarTypeTop5 = {
   },
 };
 
-export const optionBarTypeLow5 = {
+const OPTION_BAR_TYPE_LOW5 = {
   indexAxis: 'y',
   elements: {
     bar: {
@@ -73,7 +73,7 @@ export const optionBarTypeLow5 = {
   },
 };
 
-export const optionBarPokemonTop15 = {
+const OPTION_BAR_POKEMON_TOP15 = {
   indexAxis: 'y',
   elements: {
     bar: {
@@ -93,7 +93,7 @@ export const optionBarPokemonTop15 = {
   },
 };
 
-export const optionBarPokemonLow15 = {
+const OPTION_BAR_POKEMON_LOW15 = {
   indexAxis: 'y',
   elements: {
     bar: {
@@ -150,7 +150,7 @@ export function BarTypeStats({ y, colors, stats }) {
         borderRadius: '10px',
       }}
     >
-      <Bar options={optionBarTypeStats} data={dataBarTypeStats} />
+      <Bar options={OPTION_BAR_TYPE_STATS} data={dataBarTypeStats} />
     </div>
   );
 }
@@ -174,7 +174,7 @@ export function BarTypeTop5({ x, y, colors, stats }) {
         borderRadius: '10px',
       }}
     >
-      <Bar options={optionBarTypeTop5} data={dataBarTypeTop5} />
+      <Bar options={OPTION_BAR_TYPE_TOP5} data={dataBarTypeTop5} />
     </div>
   );
 }
@@ -198,7 +198,7 @@ export function BarTypeLow5({ x, y, colors, stats }) {
         borderRadius: '10px',
       }}
     >
-      <Bar options={optionBarTypeLow5} data={dataBarTypeLow5} />
+      <Bar options={OPTION_BAR_TYPE_LOW5} data={dataBarTypeLow5} />
     </div>
   );
 }
@@ -222,7 +222,7 @@ export function BarPokemonTop15({ x, y, colors, stats }) {
         borderRadius: '10px',
       }}
     >
-      <Bar options={optionBarPokemonTop15} data={dataBarPokemonTop15} />
+      <Bar options={OPTION_BAR_POKEMON_TOP15} data={dataBarPokemonTop15} />
     </div>
   );
 }
@@ -246,7 +246,7 @@ export function BarPokemonLow15({ x, y, colors, stats }) {
         borderRadius: '10px',
       }}
     >
-      <Bar options={optionBarPokemonLow15} data={dataBarPokemonLow15} />
+      <Bar options={OPTION_BAR_POKEMON_LOW15} data={dataBarPokemonLow15} />
     </div>
   );
 }
