b86ed9b9fe400b147c1495bdea3d1f180c5c8e5d
bgeun <papoke22@gmail.com>
2022-05-05 17:48:03 +0900

    [Feature] 랜딩 페이지 취지, 목적

--- front/src/pages/LandingPage/components/Section/components/InfoSection.js
+++ front/src/pages/LandingPage/components/Section/components/InfoSection.js
@@ -5,8 +5,9 @@ import {
 	Card,
 	Grid,
 	CardMedia,
+	CardContent,
 	Typography,
-	Button,
+	Divider,
 } from '@mui/material';
 
 import ImgSrc from '../../../../../core/constants/ImgSrc';
@@ -45,15 +46,102 @@ function InfoSection() {
 							포인트로 빵을 구매하고 포켓몬 스티커를 모아보세요! <br />
 							출석체크와 오박사의 퀴즈를 풀어 포인트를 얻을 수 있습니다.
 						</Typography>
-						<Button
-							variant='contained'
-							color='success'
-							size='large'
-							sx={{ display: 'flex', margin: 'auto', marginTop: '30px' }}
-						>
-							로그인
-						</Button>
 					</Box>
+				</Grid>
+			</Grid>
+			<Grid container spacing={3}>
+				<Grid
+					item
+					xs={12}
+					md={12}
+					sx={{ display: 'flex', justifyContent: 'center', marginTop: '30px' }}
+				>
+					<Typography variant='h5'>서비스 취지 & 목적</Typography>
+				</Grid>
+				<Divider sx={{ color: 'black', width: '100%', border: '1px solid' }} />
+				<Grid item xs={4} md={4}>
+					<Card>
+						<CardMedia
+							sx={{ height: '180px' }}
+							component='img'
+							image='purpose1.jpg'
+						/>
+						<CardContent>
+							<Typography variant='body1'>
+								마트 오픈시간 전 포켓몬 빵 구입 줄 선 시민들 -중앙일보
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+				<Grid item xs={4} md={4}>
+					<Card>
+						<CardMedia
+							sx={{ height: '180px' }}
+							component='img'
+							image='purpose2.png'
+						/>
+						<CardContent sx={{ backgroundColor: '#EEF2FF' }}>
+							<Typography variant='body1'>
+								MZ세대 몰려 품절대란, ‘포켓몬빵’ 검색량 분석 - 중앙일보
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+				<Grid item xs={4} md={4}>
+					<Card>
+						<CardMedia
+							sx={{ height: '180px' }}
+							component='img'
+							image='purpose3.jpg'
+						/>
+						<CardContent sx={{ backgroundColor: '#FFFFFF' }}>
+							<Typography variant='body1'>
+								포켓몬빵 40일만에 1000만개 팔렸다… - 이데일리
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+				<Grid item xs={4} md={4}>
+					<Card>
+						<CardMedia
+							sx={{ height: '180px' }}
+							component='img'
+							image='purpose4.jpg'
+						/>
+						<CardContent sx={{ backgroundColor: '#EEF2FF' }}>
+							<Typography variant='body1'>
+								“포켓몬빵이 인질로 잡혔다”…끼워팔기 상술 비난 - 헤럴드경제
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+				<Grid item xs={4} md={4}>
+					<Card>
+						<CardMedia
+							sx={{ height: '180px' }}
+							component='img'
+							image='purpose5.jpg'
+						/>
+						<CardContent sx={{ backgroundColor: '#FFFFFF' }}>
+							<Typography variant='body1'>
+								포켓몬 빵으로 유인…초등생 추행한 60대 구속 - SBS
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+				<Grid item xs={4} md={4}>
+					<Card>
+						<CardMedia
+							sx={{ height: '180px' }}
+							component='img'
+							image='purpose6.png'
+						/>
+						<CardContent sx={{ backgroundColor: '#EEF2FF' }}>
+							<Typography variant='body1'>
+								포켓몬빵 유행이 불러온 새로운 사기범죄 - jtbc
+							</Typography>
+						</CardContent>
+					</Card>
 				</Grid>
 			</Grid>
 		</Container>
