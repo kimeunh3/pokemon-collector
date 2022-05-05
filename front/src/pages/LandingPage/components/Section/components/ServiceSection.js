887c39e43fd24b9d87409e67163f84d3e1ebb7c5
singahyeon12 <singahyeon12@naver.com>
2022-05-05 18:36:43 +0900

    [Feature] 메인페이지 서비스섹션 내용작성

--- front/src/pages/LandingPage/components/Section/components/ServiceSection.js
+++ front/src/pages/LandingPage/components/Section/components/ServiceSection.js
@@ -32,7 +32,15 @@ function ServiceSection() {
           margin: '100px 100px 150px 100px',
         }}
       >
-        <Card style={{ maxWidth: '500px', justifySelf: 'center' }}>
+        <Card
+          style={{
+            maxWidth: '500px',
+            justifySelf: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}
+        >
           <CardMedia
             component='img'
             height='140'
@@ -41,19 +49,28 @@ function ServiceSection() {
           />
           <CardContent>
             <Typography gutterBottom variant='h5' component='div'>
-              Lizard
+              포켓몬빵 뽑기
             </Typography>
             <Typography variant='body2' color='text.secondary'>
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
+              1000포인트를 지불하고 7가지 종류의 포켓몬빵 중 하나를 구매하면
+              랜덤으로 포켓몬 스티커를 뽑을 수 있습니다.
+              <br />
+              수집한 포켓몬의 속성, 능력치와 같은 상세 정보를 확인할 수 있어요!
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size='small'>Share</Button>
-            <Button size='small'>Learn More</Button>
+            <Button size='small'>포켓몬빵 뽑으러 가기</Button>
           </CardActions>
         </Card>
-        <Card style={{ maxWidth: '500px', justifySelf: 'center' }}>
+        <Card
+          style={{
+            maxWidth: '500px',
+            justifySelf: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}
+        >
           <CardMedia
             component='img'
             height='140'
@@ -62,19 +79,28 @@ function ServiceSection() {
           />
           <CardContent>
             <Typography gutterBottom variant='h5' component='div'>
-              Lizard
+              오박사의 퀴즈
             </Typography>
             <Typography variant='body2' color='text.secondary'>
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
+              오박사의 포켓몬 실루엣 퀴즈를 풀고 포켓몬빵을 구매할 수 있는
+              포인트를 얻을 수 있습니다.
+              <br />
+              하루에 세 번! 최대한 많은 포인트를 획득해보세요!
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size='small'>Share</Button>
-            <Button size='small'>Learn More</Button>
+            <Button size='small'>퀴즈 풀러 가기</Button>
           </CardActions>
         </Card>
-        <Card style={{ maxWidth: '500px', justifySelf: 'center' }}>
+        <Card
+          style={{
+            maxWidth: '500px',
+            justifySelf: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}
+        >
           <CardMedia
             component='img'
             height='140'
@@ -83,16 +109,16 @@ function ServiceSection() {
           />
           <CardContent>
             <Typography gutterBottom variant='h5' component='div'>
-              Lizard
+              출석체크
             </Typography>
             <Typography variant='body2' color='text.secondary'>
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
+              24시간에 한 번, 1000포인트를 받을 수 있습니다.
+              <br />
+              자주 방문해서 원하는 포켓몬을 수집해 보세요!
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size='small'>Share</Button>
-            <Button size='small'>Learn More</Button>
+            <Button size='small'>출석체크하러 가기</Button>
           </CardActions>
         </Card>
       </div>
@@ -109,7 +135,15 @@ function ServiceSection() {
           margin: '100px 100px 0 100px',
         }}
       >
-        <Card style={{ maxWidth: '500px', justifySelf: 'center' }}>
+        <Card
+          style={{
+            maxWidth: '500px',
+            justifySelf: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}
+        >
           <CardMedia
             component='img'
             height='140'
@@ -118,19 +152,27 @@ function ServiceSection() {
           />
           <CardContent>
             <Typography gutterBottom variant='h5' component='div'>
-              Lizard
+              포켓몬 통계
             </Typography>
             <Typography variant='body2' color='text.secondary'>
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
+              우리에게 친숙한 포켓몬을 통계를 통해 더 자세히 알아보세요
+              <br />
+              몰랐던 정보를 발견할 수 있습니다!
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size='small'>Share</Button>
-            <Button size='small'>Learn More</Button>
+            <Button size='small'>포켓몬 통계 보러 가기</Button>
           </CardActions>
         </Card>
-        <Card style={{ maxWidth: '500px', justifySelf: 'center' }}>
+        <Card
+          style={{
+            maxWidth: '500px',
+            justifySelf: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}
+        >
           <CardMedia
             component='img'
             height='140'
@@ -139,19 +181,27 @@ function ServiceSection() {
           />
           <CardContent>
             <Typography gutterBottom variant='h5' component='div'>
-              Lizard
+              포켓몬 도감
             </Typography>
             <Typography variant='body2' color='text.secondary'>
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
+              도감을 통해 수집한 포켓몬을 볼 수 있습니다
+              <br />
+              포켓몬을 클릭하면 포켓몬의 상세 정보를 확인할 수 있어요!
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size='small'>Share</Button>
-            <Button size='small'>Learn More</Button>
+            <Button size='small'>포켓몬 도감 보러 가기</Button>
           </CardActions>
         </Card>
-        <Card style={{ maxWidth: '500px', justifySelf: 'center' }}>
+        <Card
+          style={{
+            maxWidth: '500px',
+            justifySelf: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}
+        >
           <CardMedia
             component='img'
             height='140'
@@ -160,16 +210,17 @@ function ServiceSection() {
           />
           <CardContent>
             <Typography gutterBottom variant='h5' component='div'>
-              Lizard
+              랭킹 시스템
             </Typography>
             <Typography variant='body2' color='text.secondary'>
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
+              랭크포인트와 포켓몬 스티커 수로 랭킹을 볼 수 있습니다.
+              <br />
+              자신의 랭킹을 확인하고, 다른 사람들은 어떤 포켓몬을 모았는지
+              확인해보세요!
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size='small'>Share</Button>
-            <Button size='small'>Learn More</Button>
+            <Button size='small'>랭킹 보러 가기</Button>
           </CardActions>
         </Card>
       </div>
