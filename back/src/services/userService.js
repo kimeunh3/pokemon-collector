ee54fa163c4e9da37543e6751d096b1d5cd511cb
[4기-2반] 장소영 <soy5360@naver.com>
2022-05-07 16:41:03 +0900

    [Modify] 닉네임 중복처리 수정

--- back/src/services/userService.js
+++ back/src/services/userService.js
@@ -19,8 +19,8 @@ class userAuthService {
         '이 이메일은 현재 사용중입니다. 다른 이메일을 입력해 주세요.';
       return { errorMessage };
     }
-    user = await User.findByNickname({ nickname });
-    if (user) {
+    // user = await User.findByNickname({ nickname });
+    if (user === user.nickname) {
       const errorMessage =
         '이 닉네임은 현재 사용중입니다. 다른 닉네임을 입력해주세요.';
       return { errorMessage };
@@ -100,15 +100,15 @@ class userAuthService {
     return loginUser;
   }
 
-  static async setUser({ userId, toUpdate, nickname }) {
+  static async setUser({ userId, toUpdate }) {
     let user = await User.findById({ userId });
 
     if (!user) {
       const errorMessage = '가입 내역이 없습니다. 다시 한 번 확인해 주세요.';
       return { errorMessage };
     }
-    user = await User.findByNickname({ nickname });
-    if (user) {
+    // user = await User.findByNickname({ nickname });
+    if (user === user.nickname) {
       const errorMessage =
         '이 닉네임은 현재 사용중입니다. 다른 닉네임을 입력해주세요.';
       return { errorMessage };
